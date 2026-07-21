f36f744140243b5161ae0fc6549bfdecffc4fc65
Zain Khizar <Zain.N.Khizar@engineering.digital.dwp.gov.uk>
2026-07-21 14:03:53 +0100

    more dynamic updates
    GitOrigin-RevId: b7eed84746b3103654978bd8ef1a8dacf02af741

--- app/filters.js
+++ app/filters.js
@@ -5,6 +5,14 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+const addFunction = govukPrototypeKit.views.addFunction
 
 // Add your filters here
+
+// Returns a date a given number of days from today, formatted like "6 May 2026"
+addFunction('daysFromNow', function (days) {
+  const date = new Date()
+  date.setDate(date.getDate() + Number(days))
+  return date.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' })
+})
 
