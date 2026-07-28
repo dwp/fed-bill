dcb139550511cbe3ac7faecb0561f03d30d07f87
Zain Khizar <Zain.N.Khizar@engineering.digital.dwp.gov.uk>
2026-07-28 15:30:46 +0100

    account and loan journey changes and fixes
    GitOrigin-RevId: a2c0ee5f7e0e47fb11f77ed716bc3485d4974575

--- app/views/v2/investigator/routes.js
+++ app/views/v2/investigator/routes.js
@@ -81,79 +81,7 @@ router.post('/'+ version +'/investigator/accounts-request', function(request, re
 })
 
 router.post('/'+ version +'/investigator/bank-account-request', function(request, response) {
-
-	// Work out whether the "All account bank statements" checkbox was ticked
-	var requestedDetails = request.session.data['holder-dets']
-	var wantsAllBankStatements = false
-	if (Array.isArray(requestedDetails)) {
-		wantsAllBankStatements = requestedDetails.indexOf('All account bank statements') !== -1
-	} else {
-		wantsAllBankStatements = requestedDetails == 'All account bank statements'
-	}
-
-	// Only show the bank statement date range page if bank statements were requested,
-	// either via "Do you want bank statements for this account?" or the
-	// "All account bank statements" option
-	if (request.session.data['bank-state'] == "Yes" || wantsAllBankStatements) {
-		response.redirect("bank-account-request-2")
-	} else {
-		response.redirect("bank-account-request-3")
-	}
-
-})
-
-router.post('/'+ version +'/investigator/bank-account-request-2', function(request, response) {
-
-
-		response.redirect("bank-account-request-3")
-
-})
-
-router.post('/'+ version +'/investigator/bank-account-request-3', function(request, response) {
-
-    // Initialize accounts array if it doesn't exist
-    if (!request.session.data['accounts']) {
-        request.session.data['accounts'] = []
-    }
-
-    // Work out which details the user actually entered so the summary only shows those
-    var hasDob = request.session.data['dob-day'] || request.session.data['dob-month'] || request.session.data['dob-year']
-    var hasAddress = request.session.data['address-line-1'] || request.session.data['address-line-2'] || request.session.data['addressTown'] || request.session.data['addressPostcode']
-    var hasBankDetails = request.session.data['sortCode'] || request.session.data['accountNo'] || request.session.data['cardNo'] || request.session.data['rollNumber']
-    var hasStatementDates = request.session.data['start-day'] || request.session.data['start-month'] || request.session.data['start-year']
-    var hasEndDate = request.session.data['end-day'] || request.session.data['end-month'] || request.session.data['end-year']
-    var hasAddrHistDates = request.session.data['addr-start-day'] || request.session.data['addr-start-month'] || request.session.data['addr-start-year']
-    var hasAddrHistEndDate = request.session.data['addr-end-day'] || request.session.data['addr-end-month'] || request.session.data['addr-end-year']
-
-    // Add current account details to the array
-    var account = {
-        fullName: request.session.data['fullName'],
-        dob: hasDob ? ((request.session.data['dob-day'] || '') + '/' + (request.session.data['dob-month'] || '') + '/' + (request.session.data['dob-year'] || '')) : '',
-        address: hasAddress ? ((request.session.data['address-line-1'] || '') + (request.session.data['address-line-2'] ? ', ' + request.session.data['address-line-2'] : '') + ', ' + (request.session.data['addressTown'] || '') + ', ' + (request.session.data['addressPostcode'] || '')) : '',
-        addressLine1: request.session.data['address-line-1'],
-        addressLine2: request.session.data['address-line-2'],
-        addressTown: request.session.data['addressTown'],
-        addressPostcode: request.session.data['addressPostcode'],
-        hasAddress: !!hasAddress,
-        sortCode: request.session.data['sortCode'],
-        accountNo: request.session.data['accountNo'],
-        cardNo: request.session.data['cardNo'],
-        rollNumber: request.session.data['rollNumber'],
-        openingInfo: request.session.data['opening-info'],
-        bankState: request.session.data['bank-state'],
-        statementDateRange: hasStatementDates ? ((request.session.data['start-day'] || '') + '/' + (request.session.data['start-month'] || '') + '/' + (request.session.data['start-year'] || '') + ' to ' + (hasEndDate ? ((request.session.data['end-day'] || '') + '/' + (request.session.data['end-month'] || '') + '/' + (request.session.data['end-year'] || '')) : 'present')) : '',
-        requestedDetails: request.session.data['holder-dets'],
-        holderInfo: request.session.data['holder-info'],
-        addressHistory: request.session.data['address-history'],
-        addressHistoryDateRange: hasAddrHistDates ? ((request.session.data['addr-start-day'] || '') + '/' + (request.session.data['addr-start-month'] || '') + '/' + (request.session.data['addr-start-year'] || '') + ' to ' + (hasAddrHistEndDate ? ((request.session.data['addr-end-day'] || '') + '/' + (request.session.data['addr-end-month'] || '') + '/' + (request.session.data['addr-end-year'] || '')) : 'present')) : '',
-        additionalInfo: request.session.data['withHint'],
-        hasBankDetails: hasBankDetails ? true : false
-    }
-
-    request.session.data['accounts'].push(account)
-
-    response.redirect("add-another-account")
-
+		response.redirect("add-another-account")
 })
 
 //router.post('/'+ version +'/investigator/add-request', function(request, response) {
@@ -183,12 +111,32 @@ router.post('/' + version + '/investigator/add-another-loan', function(request, 
     var addAnother = request.session.data['add-another']
     if (addAnother == "Yes") {
         delete request.session.data['loanAccount-name']
-        delete request.session.data['sah-day']
-        delete request.session.data['sah-month']
-        delete request.session.data['sah-year']
-        delete request.session.data['eah-day']
-        delete request.session.data['eah-month']
-        delete request.session.data['eah-year']
+        delete request.session.data['dob-day']
+        delete request.session.data['dob-month']
+        delete request.session.data['dob-year']
+        delete request.session.data['address-line-1']
+        delete request.session.data['address-line-2']
+        delete request.session.data['addressTown']
+        delete request.session.data['addressPostcode']
+        delete request.session.data['accountNo']
+        delete request.session.data['bank-state']
+        delete request.session.data['bank-start-day']
+        delete request.session.data['bank-start-month']
+        delete request.session.data['bank-start-year']
+        delete request.session.data['bank-end-day']
+        delete request.session.data['bank-end-month']
+        delete request.session.data['bank-end-year']
+        delete request.session.data['account-info']
+        delete request.session.data['loan-info']
+        delete request.session.data['holder-info']
+        delete request.session.data['address-history']
+        delete request.session.data['addr-start-day']
+        delete request.session.data['addr-start-month']
+        delete request.session.data['addr-start-year']
+        delete request.session.data['addr-end-day']
+        delete request.session.data['addr-end-month']
+        delete request.session.data['addr-end-year']
+        delete request.session.data['additional-loan-info']
         delete request.session.data['add-another']
         response.redirect("add-subject-financial-loans")
     } else {
@@ -294,6 +242,52 @@ router.post('/' + version + '/investigator/add-another-account', function(reques
             response.redirect("add-subject-financial-accounts")
         }
     } else {
+        // Initialize accounts array if it doesn't exist
+        if (!request.session.data['accounts']) {
+            request.session.data['accounts'] = []
+        }
+
+        // Work out which details the user actually entered so the summary only shows those
+        var hasDob = request.session.data['dob-day'] || request.session.data['dob-month'] || request.session.data['dob-year']
+        var hasAddress = request.session.data['address-line-1'] || request.session.data['address-line-2'] || request.session.data['addressTown'] || request.session.data['addressPostcode']
+        var hasBankDetails = request.session.data['sortCode'] || request.session.data['accountNo'] || request.session.data['cardNo'] || request.session.data['rollNumber']
+        var hasStatementDates = request.session.data['start-day'] || request.session.data['start-month'] || request.session.data['start-year']
+        var hasEndDate = request.session.data['end-day'] || request.session.data['end-month'] || request.session.data['end-year']
+        var hasTransactionDates = request.session.data['bank-start-day'] || request.session.data['bank-start-month'] || request.session.data['bank-start-year']
+        var hasTransactionEndDate = request.session.data['bank-end-day'] || request.session.data['bank-end-month'] || request.session.data['bank-end-year']
+        var hasAddrHistDates = request.session.data['addr-start-day'] || request.session.data['addr-start-month'] || request.session.data['addr-start-year']
+        var hasAddrHistEndDate = request.session.data['addr-end-day'] || request.session.data['addr-end-month'] || request.session.data['addr-end-year']
+
+        // Add current account details to the array
+        var account = {
+            fullName: request.session.data['fullName'],
+            dob: hasDob ? ((request.session.data['dob-day'] || '') + '/' + (request.session.data['dob-month'] || '') + '/' + (request.session.data['dob-year'] || '')) : '',
+            address: hasAddress ? ((request.session.data['address-line-1'] || '') + (request.session.data['address-line-2'] ? ', ' + request.session.data['address-line-2'] : '') + ', ' + (request.session.data['addressTown'] || '') + ', ' + (request.session.data['addressPostcode'] || '')) : '',
+            addressLine1: request.session.data['address-line-1'],
+            addressLine2: request.session.data['address-line-2'],
+            addressTown: request.session.data['addressTown'],
+            addressPostcode: request.session.data['addressPostcode'],
+            hasAddress: !!hasAddress,
+            sortCode: request.session.data['sortCode'],
+            accountNo: request.session.data['accountNo'],
+            cardNo: request.session.data['cardNo'],
+            rollNumber: request.session.data['rollNumber'],
+            openingInfo: request.session.data['opening-info'],
+            bankState: request.session.data['bank-state'],
+            statementDateRange: hasStatementDates ? ((request.session.data['start-day'] || '') + '/' + (request.session.data['start-month'] || '') + '/' + (request.session.data['start-year'] || '') + ' to ' + (hasEndDate ? ((request.session.data['end-day'] || '') + '/' + (request.session.data['end-month'] || '') + '/' + (request.session.data['end-year'] || '')) : 'present')) : '',
+            transactionBalances: request.session.data['bank-state'],
+            transactionDateRange: hasTransactionDates ? ((request.session.data['bank-start-day'] || '') + '/' + (request.session.data['bank-start-month'] || '') + '/' + (request.session.data['bank-start-year'] || '') + ' to ' + (hasTransactionEndDate ? ((request.session.data['bank-end-day'] || '') + '/' + (request.session.data['bank-end-month'] || '') + '/' + (request.session.data['bank-end-year'] || '')) : 'present')) : '',
+            requestedDetails: request.session.data['account-info'],
+            loanInfo: request.session.data['loan-info'],
+            holderInfo: request.session.data['holder-info'],
+            addressHistory: request.session.data['address-history'],
+            addressHistoryDateRange: hasAddrHistDates ? ((request.session.data['addr-start-day'] || '') + '/' + (request.session.data['addr-start-month'] || '') + '/' + (request.session.data['addr-start-year'] || '') + ' to ' + (hasAddrHistEndDate ? ((request.session.data['addr-end-day'] || '') + '/' + (request.session.data['addr-end-month'] || '') + '/' + (request.session.data['addr-end-year'] || '')) : 'present')) : '',
+            additionalInfo: request.session.data['withHint'],
+            hasBankDetails: hasBankDetails ? true : false
+        }
+
+        request.session.data['accounts'].push(account)
+
         if (requestType == "Specific transaction details"){
             response.redirect("check-answers-transaction")
         }
